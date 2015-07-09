7f84be2427588a44da9efc619cebf9d8b5eb9390
jmeinlschmidt <jakub@meinlschmidt.com>
2015-07-09 13:07:39 +0200

    model edits

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,6 @@ class Note < ActiveRecord::Base
 	belongs_to :user
 
 	validates :content, presence: true, length: {
-		minimum: 1,
 		maximum: 255
 	}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20150528162147) do
 
   create_table "notes", force: :cascade do |t|
     t.text     "content",    limit: 65535
-    t.integer  "owner",      limit: 4
+    t.integer  "user_id",    limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
