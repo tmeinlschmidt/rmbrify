554b8955a565c4bf0c55c5f48427d6264bad2293
Tom Meinlschmidt <tomas@meinlschmidt.com>
2015-07-18 01:25:16 +0200

    deployment enabled capistrano/bundler rvm deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,8 @@
+set :rails_env, :production
+
+set :stage, :production
+set :branch, 'production'
+
+set :rvm_ruby_version, "2.1.5"
+
+server 'orin.meinlschmidt.org',  user: 'znouza', roles: %w{db web app}
