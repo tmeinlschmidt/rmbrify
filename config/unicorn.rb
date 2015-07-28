4e10d6473ef677aec576179337ea257139f37f1e
jmeinlschmidt <jakub@meinlschmidt.com>
2015-07-28 15:06:45 +0200

    unicorn changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -38,7 +38,7 @@ pid "/home/jakub/projects/rails/rmbrify/shared/tmp/pids/unicorn.pid"
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
 stderr_path "/home/jakub/projects/rails/rmbrify/shared/log/unicorn.stderr.log"
-stdout_path "/home/jakub/projects/rails/rmbrify/app/shared/log/unicorn.stdout.log"
+stdout_path "/home/jakub/projects/rails/rmbrify/shared/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
